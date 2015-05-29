3047b01b89c1c142d84b6a818005ce33c454238c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-05-29 16:54:13 +0530

    Implement particles in intro
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    // Load particles.js
+    particlesJS.load("intro", "js/particles.json", function () {
+        console.log("loaded particles");
+    });
+
+});
