8f7e367b80a01e016c8d97a3604fc389a842b7ab
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-06-01 02:47:52 +0530

    Add the "particles" egg
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 window.addEventListener("DOMContentLoaded", function () {
 
-    // Load particles.js
-    particlesJS.load("main", "js/particles.json", function () {
-        console.log("loaded particles");
+
+    // Add the "particles" egg
+    hooker.add("particles", function (body) {
+        if (body.querySelector("#particlesScript")) {
+            console.log("Particles are already live!");
+        } else {
+            var res = false,
+                scr = document.createElement("script");
+
+            scr.setAttribute("type", "text/javascript");
+            scr.setAttribute("id", "particlesScript");
+            scr.setAttribute("src", "/vendor/particles.js/particles.min.js");
+
+            scr.onload = scr.onreadystatechange = function () {
+                if (!res && (!this.readyState || this.readyState === "complete")) {
+                    res = true;
+                    console.log("Loading particles configuration...");
+                    particlesJS.load("main", "js/particles.json", function () {
+                        document.querySelector("body > canvas").setAttribute("style", "z-index: -1;");
+                        console.log("Triggerred particles animation!");
+                    });
+                }
+            };
+            console.log("Loading particlesJS...");
+            body.appendChild(scr);
+        }
     });
 
 });
